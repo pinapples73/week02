3005db327d1b7fedbf2436775bc73bedb0082c1f
Gordon Mitchell <gordymitch@gmail.com>
2020-01-13 17:49:02 +0000

    added comments to non working code

--- day_01/lab/lab_library_class.rb
+++ day_01/lab/lab_library_class.rb
@@ -1,3 +1,6 @@
-class Library
+class LibraryInventory
 
 end
+
+
+# I CANT GET ANY FURTHER AS I KEEP GETTING A SUPERCLASS MISMATCH - i can see whats wrong
--- day_01/lab/specs/lab_library_class_spec.rb
+++ day_01/lab/specs/lab_library_class_spec.rb
@@ -4,7 +4,8 @@ require_relative('../lab_library_class')
 Minitest::Reporters.use!
 Minitest::Reporters::SpecReporter.new
 
-
-class Library < MiniTest::Test
+class LibraryInventory < MiniTest::Test
 
 end
+
+# I CANT GET ANY FURTHER AS I KEEP GETTING A SUPERCLASS MISMATCH - i can see whats wrong
